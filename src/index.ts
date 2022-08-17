b67dd3af356ddc1a629bf91311ff8d6569abe568
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-08-17 11:35:06 -0400

    more routes

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,27 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 8080;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// define a route handler for the default home page
+// HOME
 app.get('/', async (req: Request, res: Response): Promise<Response> => res.status(200).send({
   message: 'Hello World!',
 }));
 
-// start the Express server
+// MORE ROUTES
+const testRouter = express.Router();
+testRouter.route('/').get((req: Request, res: Response) => {
+  res.status(200)
+    .send({ message: 'hello test' });
+});
+testRouter.route('/:testId').get((req: Request, res: Response) => {
+  res.status(200)
+    .send({ message: `hello user id ${req.params.testId}` });
+});
+app.use('/test', testRouter);
+
+// START SERVER
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
