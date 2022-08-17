812ca002d88e1db13bed06c80c8c44b84fe960cf
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-08-17 14:01:25 -0400

    get tokens

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,30 @@ import request from 'request';
 // URLS
 const SOLSCAN = 'https://public-api.solscan.io';
 const SOLSCAN_TOKEN_HOLDER = `${SOLSCAN}/token/holders`;
+const SOLSCAN_TOKEN_META = `${SOLSCAN}/token/meta`;
+const SOLSCAN_ACCOUNT_TOKENS = `${SOLSCAN}/account/tokens`;
 
-const makeAPICall = (url:string) => new Promise((resolve, reject) => {
+const getOwnerOfToken = (tokenAddress:string) => new Promise((resolve, reject) => {
+  const url = `${SOLSCAN_TOKEN_HOLDER}?tokenAddress=${tokenAddress}`;
   request(url, { json: true }, (err, res, body) => {
     if (err) reject(err);
     resolve(body);
+  });
+});
+
+const getTokensForAddress = (walletAddress:string) => new Promise((resolve, reject) => {
+  const url = `${SOLSCAN_ACCOUNT_TOKENS}?account=${walletAddress}`;
+  request(url, { json: true }, (err, res, body) => {
+    if (err) reject(err);
+    resolve({ walletAddress, tokens: body });
+  });
+});
+
+const getMetaForToken = (tokenAddress:string) => new Promise((resolve, reject) => {
+  const url = `${SOLSCAN_TOKEN_META}?tokenAddress=${tokenAddress}`;
+  request(url, { json: true }, (err, res, body) => {
+    if (err) reject(err);
+    resolve({ tokenAddress, meta: body });
   });
 });
 
@@ -17,20 +36,34 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// HOME
 app.get('/owners', async (req: Request, res: Response) => {
   const tokens: string[] = req.body.tokenAddresses;
-
-  const promises = tokens.map((tokenAddress:string) => {
-    const url = `${SOLSCAN_TOKEN_HOLDER}?tokenAddress=${tokenAddress}`;
-
-    return makeAPICall(url);
-  });
+  const promises = tokens.map((tokenAddress:string) => getOwnerOfToken(tokenAddress));
   const resp = await Promise.all(promises);
   const owners = resp.map((response: any) => response.data.map((holder:any) => holder.owner));
+  res.status(200).send({
+    walletAddresses: owners.flat(1),
+  });
+});
+
+app.get('/tokens', async (req: Request, res: Response) => {
+  const tokens: string[] = req.body.walletAddresses;
+  const tokenReqs = tokens.map((walletAddress:string) => getTokensForAddress(walletAddress));
+  const tokensForAllWallets = await Promise.all(tokenReqs);
+
+  // eslint-disable-next-line max-len
+  const completeTokenData = tokensForAllWallets.map(async (tokensPerWallet: {wallet:string, tokens:{tokenAddress:string}[]}) => {
+    const tokenAddresses = tokensPerWallet.tokens.map((token:any) => token.tokenAddress);
+    const metaReqs = tokenAddresses.map((tokenAddress:string) => getMetaForToken(tokenAddress));
+    const allTokenMeta = await Promise.all(metaReqs);
+    return {
+      walletAddress: tokensPerWallet.wallet,
+      allTokenMeta,
+    };
+  });
 
   res.status(200).send({
-    owners,
+    completeTokenData,
   });
 });
 
